06f289662cebffbffdfba7a2d6a5aeabe068b36d
Nimit <nimitharia007@gmail.com>
2025-07-25 04:07:37 +0530

    feat: Add CRUD operations for groups including create, read, update, and delete functionalities

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,10 +3,18 @@ import { registerCreateRuleTool } from "./createRule.js";
 import { registerGetRulesTool } from "./getRules.js";
 import { registerUpdateRuleTool } from "./updateRule.js";
 import { registerDeleteRuleTool } from "./deleteRule.js";
+import { registerCreateGroupTool } from "./createGroup.js";
+import { registerGetGroupsTool } from "./getGroups.js";
+import { registerUpdateGroupTool } from "./updateGroup.js";
+import { registerDeleteGroupTool } from "./deleteGroup.js";
 
 export function registerTools(server: McpServer) {
   registerCreateRuleTool(server);
   registerGetRulesTool(server);
   registerUpdateRuleTool(server);
   registerDeleteRuleTool(server);
+  registerCreateGroupTool(server);
+  registerGetGroupsTool(server);
+  registerUpdateGroupTool(server);
+  registerDeleteGroupTool(server);
 }
